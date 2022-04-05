2fad6ea1c49b009f9f189fd3c991ccb8aecb7fce
bie71 <bie77.habibi@gmail.com>
2022-04-05 19:22:13 +0700

    portfolio tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '12px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#1e293b',
+      },
+      screens: {
+        '2xl': '1320px'
+      },
+    },
+  },
+  plugins: [],
+}

dfd6df67818ec553634a66271ad5ac0f10de4b3f
bie7 <bie77.habibi@gmail.com>
2022-04-12 00:31:14 +0700

    menambahkan fitur dark mode

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 // navbar fixed
 window.onscroll = () => {
-   const header = document.querySelector('header')
-   const fixedNav = header.offsetTop
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-   if (window.scrollY > fixedNav) {
-      header.classList.add('navbar-fixed')
-   } else {
-      header.classList.remove('navbar-fixed')
-   }
-}
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+  }
+};
 
 // hamburger
 
-const hamburger = document.querySelector('#hamburger')
-const navMenu = document.querySelector('#nav-menu')
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
-hamburger.addEventListener('click', () => {
-   hamburger.classList.toggle('hamburger-active')
-   navMenu.classList.toggle('hidden')
-})
+// window click
+
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// dark mode toggle
+
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// sesuaikan mode
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
